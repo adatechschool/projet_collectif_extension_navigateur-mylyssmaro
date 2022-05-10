35e5f82879baef3fe0b143b45968e2d823ed75cd
Marine <marine.lenoir@hotmail.fr>
2022-05-10 12:26:37 +0200

    Fonctionnement 2 boutons ok

--- Extension Grumpy Cat/ExtensionGrumpyCatMAJ9-05/background.js
+++ Extension Grumpy Cat/ExtensionGrumpyCatMAJ9-05/background.js
@@ -5,3 +5,12 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log(`the url is ${url}`);
 });
 
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === "Send Message to the BackEnd"){
+    chrome.storage.sync.get("url", url_input => {
+      console.log(url_input)
+    })
+  }
+})
+
+// let url = "https://how-do-it.com/wp-content/uploads/2018/03/Grumpy-Cat-9.jpg";
--- Extension Grumpy Cat/ExtensionGrumpyCatMAJ9-05/popup.js
+++ Extension Grumpy Cat/ExtensionGrumpyCatMAJ9-05/popup.js
@@ -1,3 +1,23 @@
+//Solution OK !!!!
+// const bouton = document.getElementById('button_image');
+// chrome.storage.sync.get("url", ({ url }) => {
+//   bouton.style.backgroundImage = "url(" + url + ")";
+// })
+
+// const submit_button = document.querySelector('#submit_button');
+
+// submit_button.addEventListener('click', () => {
+//   let url_input = document.getElementById('url_input').value;
+//   bouton.style.backgroundImage = "url(" + url_input + ")";
+//   console.log(url_input);
+// })
+
+// bouton.addEventListener('click', () => {
+//   console.log("Bouton 2")
+// })
+
+
+
 
 const bouton = document.getElementById('button_image');
 chrome.storage.sync.get("url", ({ url }) => {
@@ -8,15 +28,12 @@ const submit_button = document.querySelector('#submit_button');
 
 submit_button.addEventListener('click', () => {
   let url_input = document.getElementById('url_input').value;
-  console.log(url_input);
   bouton.style.backgroundImage = "url(" + url_input + ")";
-  // chrome.storage.sync.set("url", ({ url }) => {
-  // })
+  console.log(url_input);
 })
 
-// bouton.addEventListener('click', () => {
-//   console.log('click somewhere');
-//   chrome.scripting.executeScript({
-//     files: ['change-image.js']
-//   });
-// })
+bouton.addEventListener('click', () => {
+  console.log("Bouton 2")
+})
+
+
